f997e7ebbdf1ada8463ad43199e2545db7e37611
charlesreign <charlesgold45@gmail.com>
2024-06-09 21:47:51 +0000

    created k8s deplot and service file

--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,6 +1,19 @@
 import React from 'react'
 
 const Reset = () => {
+    fetch("http://pixagram")
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+            throw response
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
     return (
         <div>
             Reset
